5b10ee04e7822a95fe9aa62541545a34225dbf08
rata sorin <ratasorin0@gmail.com>
2021-02-25 21:22:28 +0200

    solved clicker

--- animation.js
+++ animation.js
@@ -1,41 +1,59 @@
 let sketcher = document.querySelector(".sketcher");
 let sketcherInfo = sketcher.getBoundingClientRect();
-let x;
-let y;
-let yCopy;
-let width = sketcherInfo.width;
-width = width - 1500;
 let appendTo = document.querySelector(".section1");
-let opacity = 0.9;
-let opacityCopy = opacity;
-let yVel = 3.5;
-let transparency = 0.015;
-var i = 0;
-sketcher.addEventListener("click", (e) => {
-  x = e.x - 100;
-  y = e.y - 50;
-  i++;
-  opacity = opacityCopy;
-  var sketcherCopy = document.createElement("img");
-  sketcherCopy.src = "/images/sketch.png";
-  sketcherCopy.classList.add("sketcher");
-  sketcherCopy.classList.add(`copy`);
-  appendTo.appendChild(sketcherCopy);
-  sketcherCopy.style.top = y + "px";
-  sketcherCopy.style.left = x + "px";
-  sketcherCopy.style.opacity = `${opacity}`;
-  sketcherCopy.style.width = width + "px";
-
-  function draw() {
-    y = y - yVel;
-    opacity = opacity - transparency;
-    sketcherCopy.style.opacity = `${opacity}`;
-    sketcherCopy.style.top = y + "px";
-    if (opacity < 0) {
-      sketcherCopy.remove();
-      return 0;
+var sketcherArr = [];
+const sketcherClass = function (x, y, src, opacity, width, yVel, transparency) {
+  this.src = src;
+  this.x = x;
+  this.y = y;
+  this.opacity = opacity;
+  this.width = width;
+  this.yVel = yVel;
+  this.transparency = transparency;
+};
+sketcherClass.prototype = {
+  sketcherCopy: undefined,
+  initializeOnClick() {
+    this.sketcherCopy = document.createElement("img");
+    this.sketcherCopy.src = this.src;
+    this.sketcherCopy.classList.add("sketcher");
+    this.sketcherCopy.classList.add(`copy`);
+    appendTo.appendChild(this.sketcherCopy);
+    this.sketcherCopy.style.top = this.y + "px";
+    this.sketcherCopy.style.left = this.x + "px";
+    this.sketcherCopy.style.opacity = `${this.opacity}`;
+    this.sketcherCopy.style.width = this.width + "px";
+  },
+  updateThisSketcher() {
+    this.y = this.y - this.yVel;
+    this.opacity = this.opacity - this.transparency;
+    this.sketcherCopy.style.opacity = `${this.opacity}`;
+    this.sketcherCopy.style.top = this.y + "px";
+    if (this.opacity <= 0) {
+      this.sketcherCopy.remove();
+      this.opacity = 0;
     }
-    requestAnimationFrame(draw);
-  }
-  draw();
+  },
+};
+sketcher.addEventListener("click", (e) => {
+  sketcherArr.push(
+    new sketcherClass(
+      e.x,
+      e.y,
+      "/images/sketch.png",
+      0.9,
+      sketcherInfo.width - 1500,
+      3.5,
+      0.015
+    )
+  );
+  sketcherArr[sketcherArr.length - 1].initializeOnClick();
 });
+
+function draw() {
+  for (var i = sketcherArr.length - 1; i >= 0; i--) {
+    sketcherArr[i].updateThisSketcher();
+  }
+  requestAnimationFrame(draw);
+}
+draw();
